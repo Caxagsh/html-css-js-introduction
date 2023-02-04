a0d428e5adf6b41eef075916ea597bcfe6d21c26
Alexander <caxagsh@gmail.com>
2023-02-04 20:04:28 +0200

    HW22 ver 2

--- src/ui/tabs.js
+++ src/ui/tabs.js
@@ -4,6 +4,8 @@ export class Tabs{
 
     #addButton;
     #showButton;
+    #formButtonColor;
+    #tableButtonColor;
 
 constructor (parenID, ){
     const parentButtonElement = document.getElementById(parenID);
@@ -12,6 +14,9 @@ constructor (parenID, ){
 
     this.#addButton = document.getElementById("add-button");
     this.#showButton = document.getElementById("show-button");
+    this.#formButtonColor = document.getElementById("add-button");
+    this.#tableButtonColor = document.getElementById("show-button");
+    // this.#formButtonColor = document.getElementsByTagName("button");
 
 }
 
@@ -19,10 +24,14 @@ showInput(formElement, tableElement){
    this.#addButton.addEventListener("click", (event) =>{
     formElement.style.display = "block";
     tableElement.style.display = "none";
+    this.#formButtonColor.style.backgroundColor = "rgb(25, 137, 228)";
+    this.#tableButtonColor.style.backgroundColor = "white";
    });
    this.#showButton.addEventListener("click", (event) =>{
     formElement.style.display = "none";
     tableElement.style.display = "block";
+    this.#tableButtonColor.style.backgroundColor = "rgb(25, 137, 228)";
+    this.#formButtonColor.style.backgroundColor = "white";
    })
 }
 
